5af9a233fe2ca9f44b94e82b749b29696442e814
Thangaraj <rare.thangarajbe@gmail.com>
2014-08-13 23:24:53 +0530

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,29 @@
+import unittest
+import present_schedulef
+from collections import OrderedDict, namedtuple
+class TestPresentation(unittest.TestCase):
+    
+    def setUp(self):
+        self.file = "C:\Users\hpadmin\Desktop\input.csv"
+        self.tschedule=6
+    def test_main(self):      
+        self.assertTrue(present_schedulef.main_setup(self.tschedule,self.file))
+        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)       
+    
+    def test_maxpresent_mincost(self):
+        
+        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)
+        del self.mydict['presenter']
+        del self.mydict['mincost_pres']
+        tup = namedtuple('tup', ['Name','Hours','cost'])
+        self.assertTrue(present_schedulef.maxpresent_mincost(self.mydict,tup,self.tschedule))
+    def test_mincost_present(self):
+        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)
+        del self.mydict['presenter']
+        tup = namedtuple('tup', ['Name','Hours','cost'])
+        self.assertTrue(present_schedulef.mincost_present(self.mydict,tup,self.tschedule))
+        #self.assertTrue(present_schedule.mincost_present(self.mydict,tup,self.tschedule))
+        
+        
+if __name__ == '__main__':
+    unittest.main()
