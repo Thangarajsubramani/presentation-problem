56da60bedde2413e3f00095eca92a3f911f89ba4
Thangaraj <rare.thangarajbe@gmail.com>
2014-08-13 23:28:14 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,5 @@
 import unittest
-import present_schedulef
+import present_schedule
 from collections import OrderedDict, namedtuple
 class TestPresentation(unittest.TestCase):
     
@@ -7,21 +7,21 @@ class TestPresentation(unittest.TestCase):
         self.file = "C:\Users\hpadmin\Desktop\input.csv"
         self.tschedule=6
     def test_main(self):      
-        self.assertTrue(present_schedulef.main_setup(self.tschedule,self.file))
-        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)       
+        self.assertTrue(present_schedule.main_setup(self.tschedule,self.file))
+        self.mydict=present_schedule.main_setup(self.tschedule,self.file)       
     
     def test_maxpresent_mincost(self):
         
-        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)
+        self.mydict=present_schedule.main_setup(self.tschedule,self.file)
         del self.mydict['presenter']
         del self.mydict['mincost_pres']
         tup = namedtuple('tup', ['Name','Hours','cost'])
-        self.assertTrue(present_schedulef.maxpresent_mincost(self.mydict,tup,self.tschedule))
+        self.assertTrue(present_schedule.maxpresent_mincost(self.mydict,tup,self.tschedule))
     def test_mincost_present(self):
-        self.mydict=present_schedulef.main_setup(self.tschedule,self.file)
+        self.mydict=present_schedule.main_setup(self.tschedule,self.file)
         del self.mydict['presenter']
         tup = namedtuple('tup', ['Name','Hours','cost'])
-        self.assertTrue(present_schedulef.mincost_present(self.mydict,tup,self.tschedule))
+        self.assertTrue(present_schedule.mincost_present(self.mydict,tup,self.tschedule))
         #self.assertTrue(present_schedule.mincost_present(self.mydict,tup,self.tschedule))
         
         
